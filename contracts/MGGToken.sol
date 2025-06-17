cbf553f6faf859facf304552e9750bae21873eb5
MimboNode <mimbonode@gmail.com>
2025-06-17 14:15:39 +0900

    Create MGGToken.sol

---
+++ contracts/MGGToken.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MGGToken is ERC20, Ownable {
+    uint256 private constant INITIAL_SUPPLY = 114_600_000_000;
+
+    
+
+    constructo {
+        _mint(msg.sender, INITIAL_SUPPLY * 10 ** decimals());
+    }
+
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+}
